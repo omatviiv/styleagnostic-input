b036a411b7e213d875dc2f6a2d0b5d536265515e
omatviiv <oleh.matviiv@gmail.com>
2023-03-07 22:11:18 +0200

    fix publish.sh

--- publish.sh
+++ publish.sh
@@ -1,7 +1,9 @@
 # -- 1 publish latest (default - latest npm tag) version --
+echo "== remove current .npmignore"
 rm .npmignore
 
 # default .npmignore
+echo "== create new .npmignore for default - latest npm tag"
 touch .npmignore
 echo "publish.sh" >> .npmignore
 echo "node_modules" >> .npmignore
@@ -11,15 +13,14 @@ echo "webpack.js" >> .npmignore
 echo "tsconfig.json" >> .npmignore
 echo "demo/*" >> .npmignore
 
+echo "== publish"
 npm publish
 
 # -- 2 publish demo version --
-rm .npmignore
-
+echo "== increment patch version in package.json"
 npm version patch
 
 # demo .npmignore
-touch .npmignore
 echo "!demo/index.tsx" >> .npmignore
 echo "!demo/style.ts" >> .npmignore
 
