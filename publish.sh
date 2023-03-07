3a10ec59fa014e771672679dc0fd5d19cbb91e55
omatviiv <oleh.matviiv@gmail.com>
2023-03-07 22:05:32 +0200

    publish script switch to whitelisting demo content with .npmignore

--- publish.sh
+++ publish.sh
@@ -9,7 +9,7 @@ echo ".eslintrc.js" >> .npmignore
 echo "jest.config.js" >> .npmignore
 echo "webpack.js" >> .npmignore
 echo "tsconfig.json" >> .npmignore
-echo "demo" >> .npmignore
+echo "demo/*" >> .npmignore
 
 npm publish
 
@@ -20,10 +20,7 @@ npm version patch
 
 # demo .npmignore
 touch .npmignore
-echo "publish.sh" >> .npmignore
-echo "node_modules" >> .npmignore
-echo ".eslintrc.js" >> .npmignore
-echo "jest.config.js" >> .npmignore
-echo "webpack.js" >> .npmignore
-echo "tsconfig.json" >> .npmignore
-echo "demo" >> .npmignore
+echo "!demo/index.tsx" >> .npmignore
+echo "!demo/style.ts" >> .npmignore
+
+# npm dist-tag add styleagnostic-input@0.0.5 demo
