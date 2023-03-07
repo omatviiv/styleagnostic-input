0b8763677451e1dfa6e50d5a1f6882587ab9cfd1
omatviiv <oleh.matviiv@gmail.com>
2023-03-07 22:32:45 +0200

    another try VERSION manipulation

--- publish.sh
+++ publish.sh
@@ -5,10 +5,11 @@ npm publish
 # -- 2 publish demo version --
 echo "== increment patch version in package.json"
 VERSION=$((npm version patch) 2>&1)
-echo "=========== VERSION = ${VERSION}"
+# VERSION:1 removes first character which position is 0
+echo "=========== VERSION = ${VERSION:1}"
 
 # demo .npmignore
 echo "!demo/index.tsx" >> .npmignore
 echo "!demo/style.ts" >> .npmignore
 
-# npm dist-tag add styleagnostic-input@0.0.5 demo
+# npm dist-tag add styleagnostic-input@$VERSION:1 demo
