64bbd78f7181272154ce11e1cbfaed478f33f96c
omatviiv <oleh.matviiv@gmail.com>
2023-03-07 22:33:52 +0200

    try to publish demo tag version

--- publish.sh
+++ publish.sh
@@ -4,12 +4,12 @@ npm publish
 
 # -- 2 publish demo version --
 echo "== increment patch version in package.json"
+# we store version into variable VERSION
 VERSION=$((npm version patch) 2>&1)
-# VERSION:1 removes first character which position is 0
-echo "=========== VERSION = ${VERSION:1}"
 
 # demo .npmignore
 echo "!demo/index.tsx" >> .npmignore
 echo "!demo/style.ts" >> .npmignore
 
-# npm dist-tag add styleagnostic-input@$VERSION:1 demo
+# VERSION:1 removes first character which position is 0
+npm dist-tag add styleagnostic-input@$VERSION:1 demo
