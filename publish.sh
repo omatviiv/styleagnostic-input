f486eb17ed9b00705a7d8020bf64d91d32fd2468
omatviiv <oleh.matviiv@gmail.com>
2023-03-07 22:52:38 +0200

    another try with npm dist-tag in bash

--- publish.sh
+++ publish.sh
@@ -4,12 +4,12 @@ npm publish
 
 # -- 2 publish demo version --
 echo "== increment patch version in package.json"
-# we store version into variable VERSION
-VERSION=$((npm version patch) 2>&1)
+# we store version into variable
+version=$((npm version patch) 2>&1)
 
 # demo .npmignore
 echo "!demo/index.tsx" >> .npmignore
 echo "!demo/style.ts" >> .npmignore
 
-PACKAGE_AND_VERSION="styleagnostic-input@${VERSION:1}"
-npm dist-tag add $PACKAGE_AND_VERSION demo
+packageName="styleagnostic-input"
+npm dist-tag add "$packageName@$version:1" demo
