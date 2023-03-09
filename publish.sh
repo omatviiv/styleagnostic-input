b9aa27f4243c49425d059e75fdd94663d843aa50
omatviiv <oleh.matviiv@gmail.com>
2023-03-09 20:40:43 +0200

    publish.sh improvements

--- publish.sh
+++ publish.sh
@@ -6,11 +6,13 @@ npm publish
 echo "== increment patch version in package.json"
 # we store version into variable
 version=$((npm version patch) 2>&1)
+packageName="styleagnostic-input"
 
 # demo .npmignore
 echo "!demo/index.tsx" >> .npmignore
 echo "!demo/style.ts" >> .npmignore
 
-npm publish
-packageName="styleagnostic-input"
-npm dist-tag add "$packageName@${version:1}" demo
+# this is how to add tag to some already published version
+# npm dist-tag add "$packageName@${version:1}" demo
+# but easier way is to simply use --tag option of the npm publish:
+npm publish --tag demo
